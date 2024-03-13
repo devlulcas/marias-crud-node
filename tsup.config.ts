97badc1327fd749f3dc9a189e7b18b9d988efb6c
maria.amorim <mariaclaraamorim221@gmail.com>
2024-03-13 16:08:06 -0300

    refactor: configurações

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+/* import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entryPoints: ['src/server.ts'],
+  format: ['esm'],
+  clean: true,
+  sourcemap: true,
+  minify: false,
+  target: 'node18',
+  tsconfig: 'tsconfig.json',
+}); */
