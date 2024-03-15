f66072ff179dddbe1e5c5611bc7b26c02fbfc393
maria.amorim <mariaclaraamorim221@gmail.com>
2024-03-15 11:06:02 -0300

    feat: novo campo para cadastro

--- src/controllers/CreateStockController.ts
+++ src/controllers/CreateStockController.ts
@@ -5,14 +5,17 @@ class CreateStockController {
   async handle(request: FastifyRequest, reply: FastifyReply) {
     try {
       // Extrai os dados da solicitação
-      const { name, category, quantity } = request.body as {
-        name: string;
-        category: string;
-        quantity: number;
-      };
+      const { name, category, quantity, barcode, description } =
+        request.body as {
+          name: string;
+          category: string;
+          quantity: number;
+          barcode: string;
+          description: string;
+        };
 
       // Verifica se todos os campos obrigatórios estão presentes
-      if (!name || !category || quantity == null) {
+      if (!name || !category || quantity == null || !barcode || !description) {
         // Se algum campo estiver ausente, retorna um erro de requisição inválida
         reply.code(400).send({ message: "Todos os campos são obrigatórios." });
         return;
@@ -24,16 +27,20 @@ class CreateStockController {
       // Cria uma instância do serviço de criação
       const stockService = new CreateStockService();
 
-      // Executa o serviço para criar um novo stock
+      // Executa o serviço para criar um novo estoque
       const stock = await stockService.execute({
         name,
         category,
         quantity,
-        status, // Passa o status calculado para o serviço
+        barcode,
+        description,
+        status,
       });
 
       // Envia a resposta de volta com os dados do cadastro recém-criado
-      reply.code(201).send({ message: "Produto criado com sucesso", data: stock });
+      reply
+        .code(201)
+        .send({ message: "Produto criado com sucesso", data: stock });
     } catch (error) {
       // Se ocorrer um erro durante o processamento da solicitação, retorna um erro interno do servidor
       console.error("Erro ao criar Produto:", error);
