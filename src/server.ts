97f8c6653c08c73c9e874db55ff475e2ff29685b
devlulcas <lucasalvesrego.contato@gmail.com>
2024-04-05 20:30:11 -0300

    feat: move instancia do fastify e outras configurações para um arquivo separado e separa entrada para servidor node comum e serverless

--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,13 @@
-// Importa o middleware de CORS do Fastify para lidar com as políticas de CORS
-import cors from "@fastify/cors";
-import Fastify, { FastifyInstance } from "fastify";
-import { routes } from "./routes";
-import replyPlugin from "./plugins/passPlugin";
+import { app } from './app.js';
 
-const baseURL = "http://localhost:3333";
-const port = process.env.PORT || 3333;
-
-const app: FastifyInstance = Fastify({ logger: true });
-
-// Configura um manipulador de erros para responder com status 400 e mensagem de erro em caso de exceção
-app.setErrorHandler((error, request, reply) => {
-  reply.code(400).send({ message: error.message });
-});
+const BASE_URL = process.env.BASE_URL || 'http://localhost:3333';
+const PORT = Number(process.env.PORT) || 3333;
 
 // Função assíncrona para iniciar o servidor
 const start = async () => {
   try {
-    // Registra o middleware de CORS para lidar com as políticas de CORS
-    await app.register(cors);
-
-    app.register(replyPlugin);
-
-    // Registra as rotas da aplicação
-    await app.register(routes, { prefix: "/api" }); // Prefixa todas as rotas com '/api'
-
-    // Inicia o servidor na porta 3333
-    await app.listen(port);
-    console.log(`Server running at ${baseURL}`);
+    await app.listen({ port: PORT });
+    console.log(`Server running at ${BASE_URL}`);
   } catch (err) {
     // Em caso de erro ao iniciar o servidor, encerra o processo
     console.error(err);
